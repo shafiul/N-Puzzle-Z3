0d7a140bb8f9965c6a8511df34025cd1d490902d
Shafiul Azam <ishafiul@gmail.com>
2015-11-17 12:05:05 -0600

    halfway through implementation

--- npuzzle.py
+++ npuzzle.py
@@ -7,9 +7,10 @@ NUM_BITS = 5
 
 class Npuzzle_Tester(object):
     """docstring for Npuzzle_Tester"""
-    def __init__(self,n, *args, **kwargs):
+    def __init__(self,n, num_run, *args, **kwargs):
         super(Npuzzle_Tester, self).__init__(*args, **kwargs)
         self.n = n # Number of cells
+        self.num_run = num_run  # Number of times loop will run
 
         self.ds = []
         self.solver = Solver()
@@ -29,8 +30,10 @@ class Npuzzle_Tester(object):
         self._create_ds()
 
     def _common_constrains_each_step(self, it):
+
         for r, row in enumerate(self.ds):
             for c, item in enumerate(row):
+
                 self.solver.add(item(it) > 0)
                 self.solver.add(item(it) <= self.maxval)
 
@@ -43,10 +46,40 @@ class Npuzzle_Tester(object):
                             other_item = self.ds[x][y]
                             self.solver.add(item(it) != other_item(it))
 
+            if it == self.num_run-1:
+                self._p('[Last Loop!]')
+            else:
+                self._apply_transitions()
+
+    def _apply_transitions(self, item, r, c,  i):
+        e = self._maxval
+        n_m_1 = self.n-1
+
+
+        def s1(neigh):
+            """
+                cur: Current Element
+            """
+            return And(neigh[1], item(i+1) == neigh[0](i), neigh[0](i+1) == e)
+
+        it_n = (self.ds[r-1][c], (r > 0),)  # r-1 >= 0; r >= 1
+        it_s = (self.ds[r+1][c], (r < n_m_1),)  # r+1 < n; r < n-1
+        it_e = (self.ds[r][c+1], (c < n_m_1),)  # 
+        it_w = (self.ds[r][c-1], (c > 0),)  #
+
+        c_empty = And((item(i) == e), Or(s1(it_n), s1(it_s), s1(it_e), s1(it_w)))
+
+        c_nonempty = And((item(i) != e), Or())
+
+        self.solver.add(Or(c_empty, c_nonempty))
+
 
     def _run(self):
-        self._p('Running...')
-        self._common_constrains_each_step(0)
+        self._p('[i] Starting run. N: {0}; loop: {1}'.format(self.n, self.num_run))
+
+        for i in range(self.num_run):
+            self._p('--- LOOP {0} ---'.format(i))
+            self._common_constrains_each_step(i)
 
         self._post_run()
 
@@ -81,5 +114,5 @@ class Npuzzle_Tester(object):
 
 if __name__ == '__main__':
     
-    tester = Npuzzle_Tester(3)
+    tester = Npuzzle_Tester(3, 2)
     tester.generate_tests()
---
+++ test.py
@@ -0,0 +1,7 @@
+i=1
+a = (i > 0, 'oka', )
+
+if a[0]:
+	print(a[1])
+else:
+	print ('no')

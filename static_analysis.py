7d93a5ef863012bbeea45e30aa8e27cc1a63e764
Shafiul Azam <ishafiul@gmail.com>
2015-11-19 16:34:09 -0600

    Encoded the lotter problem more generic way.

--- static_analysis.py
+++ static_analysis.py
@@ -1,7 +1,70 @@
 #!/usr/bin/env python
 from z3 import *
 
+
 def sa_lottery(num_bits):
+    """
+        This solution goes through all the 2^3 paths and checks for answer.
+    """
+
+    length = 4
+    prize = 0
+
+    param_num_bits = 3  # Number of bits for a,b,c vars below
+
+    a = BitVec('a', param_num_bits)
+    b = BitVec('b', param_num_bits)
+    c = BitVec('c', param_num_bits)
+
+    p = Function('position', BitVecSort(num_bits), BitVecSort(num_bits))    # Position Variable
+
+    s = Solver()
+
+    s.add(a > -1, a < 2)  # {0, 1}
+    s.add(b > -1, b < 2)  # {0, 1}
+    s.add(c > -1, c < 2)  # {0, 1}
+
+    s.add(p(0) == 0)    #  position=0;
+
+    s.add(Or(
+        And(a == 1, p(1) == ((p(0) + length - 2)% length)),
+        And(a == 0, p(1) == ((p(0) + 1)% length))
+        ))
+
+    s.add(Or(
+            And(b == 1, p(2) == ((p(1) + 1)% length)),
+            And(b == 0, p(2) == ((p(1) + length - 2)% length))
+            ))
+
+    s.add(Or(
+            And(c == 1, p(3) == ((p(2) + length - 2)% length)),
+            And(c == 0, p(3) == ((p(2) + 1)% length))
+            ))
+
+
+    s.add(p(3) == prize)
+
+    # Sat Check
+
+    while True:
+
+        if s.check() == sat:
+            m = s.model()
+            print('sat: a,b,c = {},{},{}'.format(m.evaluate(a), m.evaluate(b), m.evaluate(c)))
+            s.add(Or(
+                    a != m.evaluate(a), b != m.evaluate(b), c != m.evaluate(c)
+                ))
+        else:
+            print('UNSAT: no more models found.')
+            break
+
+
+    
+
+def sa_lottery_individual_encoding(num_bits):
+    """
+        This was my first attempt to encode only two paths, as asked in the question.
+    """
 
     length = 4
     prize = 2
@@ -15,10 +78,6 @@ def sa_lottery(num_bits):
     p = Function('position', BitVecSort(num_bits), BitVecSort(num_bits))    # Position Variable
 
     s = Solver()
-
-    # s.add(a > -1, a < 2)  # {0, 1}
-    # s.add(b > -1, b < 2)  # {0, 1}
-    # s.add(c > -1, c < 2)  # {0, 1}
 
     s.add(p(0) == 0)    #  position=0;
 
@@ -69,10 +128,6 @@ def sa_lottery(num_bits):
         print('Unsat in second path')
 
     s.pop()
-
-
-
-
 
 
 if __name__ == '__main__':
